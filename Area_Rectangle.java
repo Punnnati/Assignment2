8dd192e740d6e22548720bf64bf751bae0e62ed4
sowmya <161058500+Punnnati@users.noreply.github.com>
2025-08-02 14:24:35 +0530

    Add files via upload

---
+++ Area_Rectangle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class Area_Rectangle{ 
+    public static void main(String[] args){ 
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the length of the Rectangle : "); 
+        int l = sc.nextInt(); 
+        System.out.print("Enter the breadth of the Rectangle : "); 
+        int b = sc.nextInt(); 
+        System.out.println("The Area of the square is : "+(l*b)); 
+    } 
+} 
+ 
